5a8d554d79f8688878d0faf4034f1452fb9dc427
aya <aya2103436@miuegypt.edu.eg>
2024-05-23 03:26:57 +0300

    code1

--- demo/src/main/java/com/example/demo/models/RoadmapSteps.java
+++ demo/src/main/java/com/example/demo/models/RoadmapSteps.java
@@ -1,5 +1,34 @@
 package com.example.demo.models;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roadmapsteps")
 public class RoadmapSteps {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @ManyToOne
+     @JsonBackReference
+    private Roadmap roadmap;
+    @OneToMany(mappedBy = "roadmapSteps")
+    private List<Resources> resources;
 }
