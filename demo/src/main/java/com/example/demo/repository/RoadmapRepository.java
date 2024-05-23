31697240e7b236989675ca3fcaffe483bcb0367f
aya <aya2103436@miuegypt.edu.eg>
2024-05-23 03:31:33 +0300

    code roadmap repo

--- demo/src/main/java/com/example/demo/repository/RoadmapRepository.java
+++ demo/src/main/java/com/example/demo/repository/RoadmapRepository.java
@@ -1,5 +1,21 @@
 package com.example.demo.repository;
 
-public class RoadmapRepository {
+import com.example.demo.models.Roadmap;
+import com.example.demo.models.Status;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
+    List<Roadmap> findAllById(Long id);
+    List<Roadmap> findByTitleContainingIgnoreCase(String title);
+    List<Roadmap> findByUserId(Long userId);
+
+    List<Roadmap> findByStatus(Status status);
+
+    @Query("SELECT r FROM Roadmap r WHERE r.user.role = 'USER'")
+    List<Roadmap> findAllByUserRoleUser();
     
 }
