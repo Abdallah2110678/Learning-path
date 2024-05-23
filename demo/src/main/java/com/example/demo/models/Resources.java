f61db7c81ff6b8daa070e480d6611337b6913c2f
esraa <esraahassann23@gmail.com>
2024-05-23 04:18:17 +0300

    code resourcessss

--- demo/src/main/java/com/example/demo/models/Resources.java
+++ demo/src/main/java/com/example/demo/models/Resources.java
@@ -1,5 +1,33 @@
 package com.example.demo.models;
 
+import com.example.demo.service.RoadmapStepsServiceImpl;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "resources")
 public class Resources {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String url;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "roadmap_steps_id", nullable = false)
+    private RoadmapSteps roadmapSteps;
 }
