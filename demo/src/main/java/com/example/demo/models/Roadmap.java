7f7b2827b4793a0b4bb7207c60c6b7cd69cc6b9f
esraa <esraahassann23@gmail.com>
2024-05-23 04:19:02 +0300

    roadmap code

--- demo/src/main/java/com/example/demo/models/Roadmap.java
+++ demo/src/main/java/com/example/demo/models/Roadmap.java
@@ -1,5 +1,44 @@
 package com.example.demo.models;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roadmap")
 public class Roadmap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
     
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+
+
+    @ManyToOne
+    private User user;
+    @OneToMany(mappedBy = "roadmap")
+    @JsonManagedReference
+    private List<RoadmapSteps> steps;
 }
