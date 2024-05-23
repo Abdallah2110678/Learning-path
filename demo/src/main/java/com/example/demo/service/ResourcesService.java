23ac9b2b6898e273a0ceade35ebe3822623eb3e7
aya <aya2103436@miuegypt.edu.eg>
2024-05-23 03:36:29 +0300

    code resource servicr

--- demo/src/main/java/com/example/demo/service/ResourcesService.java
+++ demo/src/main/java/com/example/demo/service/ResourcesService.java
@@ -1,5 +1,8 @@
 package com.example.demo.service;
 
-public class ResourcesService {
-    
+import com.example.demo.models.Resources;
+
+public interface ResourcesService {
+    Resources createResource(Resources resource);
+    void deleteResourceById(Long id);
 }
