48f5213b67f85bc0fd18a99e92106ae2bc84dda1
aya <aya2103436@miuegypt.edu.eg>
2024-05-23 03:37:20 +0300

    codeee

--- demo/src/main/java/com/example/demo/service/ResourcesServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/ResourcesServiceImpl.java
@@ -1,5 +1,23 @@
 package com.example.demo.service;
 
-public class ResourcesServiceImpl {
-    
+import com.example.demo.models.Resources;
+import com.example.demo.repository.ResourcesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResourcesServiceImpl implements ResourcesService {
+
+    @Autowired
+    private ResourcesRepository resourcesRepository;
+
+    @Override
+    public Resources createResource(Resources resource) {
+        return resourcesRepository.save(resource);
+    }
+
+    @Override
+    public void deleteResourceById(Long id) {
+        resourcesRepository.deleteById(id);
+    }
 }
